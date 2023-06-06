def81f778022a6447b0d04ad9e871f5bbc586567
Mango-DevopsEngineer <nakkabhavani1996@gmail.com>
2023-06-06 12:15:06 +0530

    printing in colors

--- 01-sample.sh
+++ 01-sample.sh
@@ -3,4 +3,4 @@ pwd
 
 yum list installed | grep vim
 
-echo -e "\e[31mHello World shold print in RED color\e[0m"
+
---
+++ 02-print.sh
@@ -0,0 +1,8 @@
+echo This shell is to print the information in different colors using echo command
+
+echo -e "\e[31mHello World shold print in RED color\e[0m"
+echo -e "\e[32mHello World shold print in Green color\e[0m"
+echo -e "\e[33mHello World shold print in Yellow color\e[0m"
+echo -e "\e[34mHello World shold print in Blue color\e[0m"
+echo -e "\e[35mHello World shold print in Magenta color\e[0m"
+echo -e "\e[36mHello World shold print in Cyan color\e[0m"
